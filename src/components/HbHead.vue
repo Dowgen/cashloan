313adc641d287c7eae754e16d6b1250505f3b039
Melody0907 <972684544@qq.com>
2017-09-18 11:36:43 +0800

    .\HbHead.vue

--- src/components/HbHead.vue
+++ src/components/HbHead.vue
@@ -21,33 +21,33 @@ export default {
 	XHeader
   },
   props: {
-	headfont: {
-		type: String,
-		default: '导航'
-	}
+    headfont: {
+        type: String,
+        default: '导航'
+    }
   },
   //实例初始化最之前，无法获取到data里的数据
   beforeCreate(){
-  	
-  	
-  },  
+
+
+  },
   //在挂载开始之前被调用
   beforeMount(){
-  	
-  
-  }, 
+
+
+  },
   //已成功挂载，相当ready()
   mounted(){
-  
-  
-	
+
+
+
   },
   //相关操作事件
   methods: {
-	
-	
-	
-      
+
+
+
+
   }
 }
 </script>
@@ -63,9 +63,9 @@ export default {
 }
 
 .headMargin{
-clear:both;
-height:50px;
-border-bottom:solid 1px #ddd;
+    clear:both;
+    height:50px;
+    border-bottom:solid 1px #ddd;
 }
 
 </style>
